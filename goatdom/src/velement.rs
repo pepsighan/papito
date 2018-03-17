0d78f02d818eee9e4915d7df99c3052260f4e27d
Sharad Chand <sharad.d.chand@gmail.com>
2018-03-17 18:26:39 +0545

    Remove Attribute if empty after taking class

--- goatdom/src/velement.rs
+++ goatdom/src/velement.rs
@@ -41,7 +41,7 @@ impl VElement {
             class,
             attrs,
             child: child.map(|it| Box::new(it)),
-            is_self_closing
+            is_self_closing,
         }
     }
 }
@@ -89,9 +89,8 @@ impl<A, B, C> From<(A, Vec<(B, C)>, VNode, bool)> for VElement where
     C: Into<CowStr> {
     fn from(item: (A, Vec<(B, C)>, VNode, bool)) -> Self {
         let tag = item.0.into();
-        let mut attrs: Attributes = item.1.into();
-        let class = swap_remove_class(&mut attrs);
-        VElement::new(tag, class, Some(attrs), Some(item.2), item.3)
+        let (class, attrs) = split_into_class_and_attrs(item.1.into());
+        VElement::new(tag, class, attrs, Some(item.2), item.3)
     }
 }
 
@@ -101,9 +100,8 @@ impl<A, B, C> From<(A, Vec<(B, C)>, VNode)> for VElement where
     C: Into<CowStr> {
     fn from(item: (A, Vec<(B, C)>, VNode)) -> Self {
         let tag = item.0.into();
-        let mut attrs: Attributes = item.1.into();
-        let class = swap_remove_class(&mut attrs);
-        VElement::new(tag, class, Some(attrs), Some(item.2), false)
+        let (class, attrs) = split_into_class_and_attrs(item.1.into());
+        VElement::new(tag, class, attrs, Some(item.2), false)
     }
 }
 
@@ -113,9 +111,8 @@ impl<A, B, C> From<(A, Vec<(B, C)>, bool)> for VElement where
     C: Into<CowStr> {
     fn from(item: (A, Vec<(B, C)>, bool)) -> Self {
         let tag = item.0.into();
-        let mut attrs: Attributes = item.1.into();
-        let class = swap_remove_class(&mut attrs);
-        VElement::new(tag, class, Some(attrs), None, item.2)
+        let (class, attrs) = split_into_class_and_attrs(item.1.into());
+        VElement::new(tag, class, attrs, None, item.2)
     }
 }
 
@@ -125,9 +122,8 @@ impl<A, B, C> From<(A, Vec<(B, C)>)> for VElement where
     C: Into<CowStr> {
     fn from(item: (A, Vec<(B, C)>)) -> Self {
         let tag = item.0.into();
-        let mut attrs: Attributes = item.1.into();
-        let class = swap_remove_class(&mut attrs);
-        VElement::new(tag, class, Some(attrs), None, false)
+        let (class, attrs) = split_into_class_and_attrs(item.1.into());
+        VElement::new(tag, class, attrs, None, false)
     }
 }
 
@@ -163,6 +159,7 @@ impl<A> From<(A, VNode)> for VElement where
     }
 }
 
-fn swap_remove_class(attrs: &mut Attributes) -> Option<ClassString> {
-    attrs.0.swap_remove("class").map(|it| it.into())
+fn split_into_class_and_attrs(mut attrs: Attributes) -> (Option<ClassString>, Option<Attributes>) {
+    let class = attrs.0.swap_remove("class").map(|it| it.into());
+    (class, if attrs.0.len() == 0 { None } else { Some(attrs) })
 }
