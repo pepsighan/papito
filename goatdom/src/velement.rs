f888a693b701c007cb337e573d6c85cb3a6a72ab
Sharad Chand <sharad.d.chand@gmail.com>
2018-03-17 16:52:37 +0545

    Added conversions from tuples to VElement

--- goatdom/src/velement.rs
+++ goatdom/src/velement.rs
@@ -63,3 +63,97 @@ impl Display for VElement {
         }
     }
 }
+
+impl<A, B> From<Vec<(A, B)>> for Attributes where
+    A: Into<CowStr>,
+    B: Into<CowStr> {
+    fn from(item: Vec<(A, B)>) -> Self {
+        Attributes(item.into_iter()
+            .map(|(k, v)| (k.into(), v.into()))
+            .collect())
+    }
+}
+
+impl<A, B, C> From<(A, Vec<(B, C)>, VNode, bool)> for VElement where
+    A: Into<CowStr>,
+    B: Into<CowStr>,
+    C: Into<CowStr> {
+    fn from(item: (A, Vec<(B, C)>, VNode, bool)) -> Self {
+        let tag = item.0.into();
+        let mut attrs: Attributes = item.1.into();
+        let class = swap_remove_class(&mut attrs);
+        VElement::new(tag, class, Some(attrs), Some(item.2), item.3)
+    }
+}
+
+impl<A, B, C> From<(A, Vec<(B, C)>, VNode)> for VElement where
+    A: Into<CowStr>,
+    B: Into<CowStr>,
+    C: Into<CowStr> {
+    fn from(item: (A, Vec<(B, C)>, VNode)) -> Self {
+        let tag = item.0.into();
+        let mut attrs: Attributes = item.1.into();
+        let class = swap_remove_class(&mut attrs);
+        VElement::new(tag, class, Some(attrs), Some(item.2), false)
+    }
+}
+
+impl<A, B, C> From<(A, Vec<(B, C)>, bool)> for VElement where
+    A: Into<CowStr>,
+    B: Into<CowStr>,
+    C: Into<CowStr> {
+    fn from(item: (A, Vec<(B, C)>, bool)) -> Self {
+        let tag = item.0.into();
+        let mut attrs: Attributes = item.1.into();
+        let class = swap_remove_class(&mut attrs);
+        VElement::new(tag, class, Some(attrs), None, item.2)
+    }
+}
+
+impl<A, B, C> From<(A, Vec<(B, C)>)> for VElement where
+    A: Into<CowStr>,
+    B: Into<CowStr>,
+    C: Into<CowStr> {
+    fn from(item: (A, Vec<(B, C)>)) -> Self {
+        let tag = item.0.into();
+        let mut attrs: Attributes = item.1.into();
+        let class = swap_remove_class(&mut attrs);
+        VElement::new(tag, class, Some(attrs), None, false)
+    }
+}
+
+impl<A> From<(A, bool)> for VElement where
+    A: Into<CowStr> {
+    fn from(item: (A, bool)) -> Self {
+        let tag = item.0.into();
+        VElement::new(tag, None, None, None, item.1)
+    }
+}
+
+impl<A> From<(A, ())> for VElement where
+    A: Into<CowStr> {
+    fn from(item: (A, ())) -> Self {
+        let tag = item.0.into();
+        VElement::new(tag, None, None, None, false)
+    }
+}
+
+impl<A> From<(A, VNode, bool)> for VElement where
+    A: Into<CowStr> {
+    fn from(item: (A, VNode, bool)) -> Self {
+        let tag = item.0.into();
+        VElement::new(tag, None, None, Some(item.1), item.2)
+    }
+}
+
+impl<A> From<(A, VNode)> for VElement where
+    A: Into<CowStr> {
+    fn from(item: (A, VNode)) -> Self {
+        let tag = item.0.into();
+        VElement::new(tag, None, None, Some(item.1), false)
+    }
+}
+
+fn swap_remove_class(attrs: &mut Attributes) -> Option<ClassString> {
+    attrs.0.swap_remove("class").map(|it| ClassString(it))
+}
