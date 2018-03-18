0dac64cb3ce1072dec9b981a0bcd8fd09f1677c6
Sharad Chand <sharad.d.chand@gmail.com>
2018-03-18 15:48:28 +0545

    Impl DOMPatch for VNode and removed ref errors from DOMRemove

--- goatdom/src/vnode.rs
+++ goatdom/src/vnode.rs
@@ -45,10 +45,27 @@ impl_conversion_to_vnode!(Text, VText);
 impl_conversion_to_vnode!(Element, VElement);
 impl_conversion_to_vnode!(List, VList);
 
+macro_rules! match_for_vnode_patch {
+    ($against:ident, $parent:ident, $old_vnode:ident, [$( $variant:ident ),*] ) => {
+        match *$against {
+            $(
+                VNode::$variant(ref mut node_like) => {
+                    if let Some(&VNode::$variant(ref old_node_like)) = $old_vnode {
+                        node_like.patch($parent, Some(old_node_like));
+                    } else {
+                        $old_vnode.remove($parent);
+                        node_like.patch($parent, None);
+                    }
+                }
+            )*
+        }
+    };
+}
+
 #[cfg(target_arch = "wasm32")]
 impl DOMPatch<VNode> for VNode {
     fn patch(&mut self, parent: &Element, old_vnode: Option<&VNode>) {
-        unimplemented!()
+        match_for_vnode_patch!(self, parent, old_vnode, [Text, Element, List]);
     }
 }
 
@@ -56,9 +73,9 @@ impl DOMPatch<VNode> for VNode {
 impl DOMRemove for VNode {
     fn remove(&self, parent: &Element) {
         match *self {
-            VNode::Text(text) => text.remove(parent),
-            VNode::Element(element) => element.remove(parent),
-            VNode::List(list) => list.remove(parent)
+            VNode::Text(ref text) => text.remove(parent),
+            VNode::Element(ref element) => element.remove(parent),
+            VNode::List(ref list) => list.remove(parent)
         }
     }
 }
