026e0783db576a4deb8fc2352b40c8c7b0aee6f9
Sharad Chand <sharad.d.chand@gmail.com>
2018-03-18 16:26:18 +0545

    Impl DOMPatch for VList

--- goatdom/src/vlist.rs
+++ goatdom/src/vlist.rs
@@ -4,7 +4,7 @@ use std::fmt::{Formatter, self};
 use indexmap::IndexMap;
 use CowStr;
 #[cfg(target_arch = "wasm32")]
-use vdiff::{DOMPatch, DOMRemove};
+use vdiff::{DOMPatch, DOMRemove, DOMReorder};
 #[cfg(target_arch = "wasm32")]
 use stdweb::web::Element;
 
@@ -54,7 +54,27 @@ impl From<Vec<VNode>> for VList {
 #[cfg(target_arch = "wasm32")]
 impl DOMPatch<VList> for VList {
     fn patch(&mut self, parent: &Element, old_vnode: Option<&VList>) {
-        unimplemented!()
+        if let Some(ref old_vnode) = old_vnode {
+            let mut old_children: IndexMap<_, _> = old_vnode.children.iter().collect();
+            for (k, v) in self.children.iter_mut() {
+                if let Some(pre_vnode) = old_children.swap_remove(k) {
+                    // Patch if any old VNode found
+                    v.patch(parent, Some(pre_vnode));
+                    // Reorder based on insertion
+                    v.reorder(parent);
+                } else {
+                    v.patch(parent, None);
+                }
+            }
+            // Remove any VNodes left out
+            for (_, v) in old_children {
+                v.remove(parent);
+            }
+        } else {
+            for (_, v) in self.children.iter_mut() {
+                v.patch(parent, None);
+            }
+        }
     }
 }
 
