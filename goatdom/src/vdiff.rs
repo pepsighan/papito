de86935fbbb15f7c7c431fee239f263183eb7a6a
Sharad Chand <sharad.d.chand@gmail.com>
2018-03-18 12:00:16 +0545

    The old_vnode can be anything which represents a node

--- goatdom/src/vdiff.rs
+++ goatdom/src/vdiff.rs
@@ -2,5 +2,7 @@ use stdweb::web::Element;
 use vnode::VNode;
 
 pub trait VDiff {
-    fn apply(&mut self, parent: &Element, old_vnode: &VNode);
+    type VNodeLike;
+
+    fn apply(&mut self, parent: &Element, old_vnode: Option<&Self::VNodeLike>);
 }
