13c7c6ae0bf30eff8d404048cd7966ee701d3049
Sharad Chand <sharad.d.chand@gmail.com>
2018-03-18 14:44:21 +0545

    VDiff is actually a patch operation. And added DOMRemove trait.

--- goatdom/src/vdiff.rs
+++ goatdom/src/vdiff.rs
@@ -1,8 +1,38 @@
 use stdweb::web::Element;
 use vnode::VNode;
 
-pub trait VDiff {
-    type VNodeLike;
+pub trait DOMPatch<T> {
+    fn patch(&mut self, parent: &Element, old_vnode: Option<&T>);
+}
 
-    fn apply(&mut self, parent: &Element, old_vnode: Option<&Self::VNodeLike>);
+pub trait DOMRemove {
+    fn remove(&self, parent: &Element);
+}
+
+impl<T, Q> DOMPatch<T> for Option<Q> where
+    Q: DOMPatch<T>,
+    T: DOMRemove {
+    fn patch(&mut self, parent: &Element, old_vnode: Option<&T>) {
+        if let Some(ref mut this) = *self {
+            this.patch(parent, old_vnode);
+        } else {
+            old_vnode.remove(parent);
+        }
+    }
+}
+
+impl<T, Q> DOMPatch<Q> for Box<T> where
+    T: DOMPatch<Q> {
+    fn patch(&mut self, parent: &Element, old_vnode: Option<&Q>) {
+        let this = &mut **self;
+        this.patch(parent, old_vnode);
+    }
+}
+
+impl<'a, T: DOMRemove> DOMRemove for Option<&'a T> {
+    fn remove(&self, parent: &Element) {
+        if let Some(ref inner) = *self {
+            inner.remove(parent);
+        }
+    }
 }
