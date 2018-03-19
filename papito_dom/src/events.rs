2e183ebd6e7b5c59db09055270f178a7a1b3004c
Sharad Chand <sharad.d.chand@gmail.com>
2018-03-19 08:52:54 +0545

    Impl conversion to DOMEventListener for stdweb provided events

--- papito_dom/src/events.rs
+++ papito_dom/src/events.rs
@@ -1,5 +1,6 @@
-use stdweb::web::{Element, EventListenerHandle};
+use stdweb::web::{Element, EventListenerHandle, IEventTarget};
 use stdweb::web::event::*;
+use std::marker::PhantomData;
 
 /// Add or remove events from the DOM
 pub trait DOMEvents {
@@ -8,14 +9,30 @@ pub trait DOMEvents {
     fn detach(&mut self);
 }
 
+/// A wrapper construct to encapsulate all events
 pub struct DOMEventListener<T, F> where
-    F: FnMut(T),
+    F: FnMut(T) + 'static,
     T: ConcreteEvent {
     listener: Option<F>,
     listener_handle: Option<EventListenerHandle>,
+    _phantom: PhantomData<T>,
 }
 
-impl<T, F> DOMEvents for DOMEventListener<T, F> {
+impl<T, F> DOMEventListener<T, F> where
+    F: FnMut(T) + 'static,
+    T: ConcreteEvent {
+    pub fn new(listener: F) -> DOMEventListener<T, F> {
+        DOMEventListener {
+            listener: Some(listener),
+            listener_handle: None,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<T, F> DOMEvents for DOMEventListener<T, F> where
+    F: FnMut(T) + 'static,
+    T: ConcreteEvent {
     fn attach(&mut self, parent: &Element) {
         let listener = self.listener.take()
             .expect("Event listener is either already attached or detached");
@@ -29,3 +46,48 @@ impl<T, F> DOMEvents for DOMEventListener<T, F> {
         listener_handle.remove();
     }
 }
+
+macro_rules! convert_to_dom_ev_listener {
+    ($( $listener:ty ),*) => {
+        $(
+            impl<F> From<F> for DOMEventListener<$listener, F> where
+                F: FnMut($listener) {
+                fn from(item: F) -> Self {
+                    DOMEventListener::new(item)
+                }
+            }
+        )*
+    };
+}
+
+convert_to_dom_ev_listener!(
+    ClickEvent,
+    DoubleClickEvent,
+    MouseDownEvent,
+    MouseUpEvent,
+    MouseMoveEvent,
+    KeyPressEvent,
+    KeyDownEvent,
+    KeyUpEvent,
+    ProgressEvent,
+    LoadStartEvent,
+    LoadEndEvent,
+    ProgressLoadEvent,
+    ProgressAbortEvent,
+    ProgressErrorEvent,
+    SocketCloseEvent,
+    SocketErrorEvent,
+    SocketOpenEvent,
+    SocketMessageEvent,
+    HashChangeEvent,
+    PopStateEvent,
+    ChangeEvent,
+    ResourceLoadEvent,
+    ResourceAbortEvent,
+    ResourceErrorEvent,
+    ResizeEvent,
+    InputEvent,
+    ReadyStateChangeEvent,
+    FocusEvent,
+    BlurEvent
+);
