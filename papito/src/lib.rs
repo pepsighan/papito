1bec95f3c46584214188d00f327acd6dbbbb8466
Sharad Chand <sharad.d.chand@gmail.com>
2018-03-25 18:04:58 +0545

    Target gated App

--- papito/src/lib.rs
+++ papito/src/lib.rs
@@ -1,10 +1,14 @@
 extern crate papito_dom;
+#[cfg(target_arch = "wasm32")]
 #[macro_use]
 extern crate stdweb;
 
 use papito_dom::prelude::VNode;
-use papito_dom::{comp, h, DOMRender, RenderRequest, Component, ComponentOf};
+use papito_dom::{comp, h, Component, ComponentOf};
+#[cfg(target_arch = "wasm32")]
 use stdweb::web::{document, Element, INonElementParentNode};
+#[cfg(target_arch = "wasm32")]
+use papito_dom::{DOMRender, RenderRequest};
 use std::ops::Deref;
 
 pub mod prelude {
@@ -13,17 +17,20 @@ pub mod prelude {
 
 pub struct App {
     vdom: VNode,
+    #[cfg(target_arch = "wasm32")]
     render_req: RenderRequest,
 }
 
 impl App {
     pub fn new<T: Component<Props=()> + 'static, C: ComponentOf<Comp=T>>() -> App {
+        #[cfg(target_arch = "wasm32")]
         js! { @(no_return)
             window.__schedule_papito_render__ = function() {};
-        }
+        };
 
         App {
             vdom: h(comp::<C::Comp>(())),
+            #[cfg(target_arch = "wasm32")]
             render_req: RenderRequest::new(|| {
                 js! { @(no_return)
                     window.__schedule_papito_render__();
@@ -32,6 +39,7 @@ impl App {
         }
     }
 
+    #[cfg(target_arch = "wasm32")]
     pub fn render<T: Into<AppRoot>>(mut self, app_root: T) {
         let app_root = app_root.into();
         // Re-renders on requests from the components
@@ -59,8 +67,10 @@ impl App {
     }
 }
 
+#[cfg(target_arch = "wasm32")]
 pub struct AppRoot(Element);
 
+#[cfg(target_arch = "wasm32")]
 impl<'a> From<&'a str> for AppRoot {
     fn from(item: &'a str) -> Self {
         AppRoot(document().get_element_by_id(item)
@@ -68,12 +78,14 @@ impl<'a> From<&'a str> for AppRoot {
     }
 }
 
+#[cfg(target_arch = "wasm32")]
 impl From<Element> for AppRoot {
     fn from(item: Element) -> Self {
         AppRoot(item)
     }
 }
 
+#[cfg(target_arch = "wasm32")]
 impl Deref for AppRoot {
     type Target = Element;
 
