bdb9bfa656c3ab62b073e94cf973c81f15eb690f
Sharad Chand <sharad.d.chand@gmail.com>
2018-04-02 17:04:45 +0545

    Removed #[component] & #[render]

--- papito_codegen/src/lib.rs
+++ papito_codegen/src/lib.rs
@@ -10,24 +10,8 @@ extern crate proc_macro2;
 use proc_macro::TokenStream;
 use syn::{Item, DeriveInput};
 
-mod component;
-mod render;
 mod events;
 mod common;
-
-#[proc_macro_attribute]
-pub fn component(_metadata: TokenStream, input: TokenStream) -> TokenStream {
-    let state: Item = syn::parse(input).unwrap();
-    let expanded = component::quote(state);
-    expanded.into()
-}
-
-#[proc_macro_attribute]
-pub fn render(_metadata: TokenStream, input: TokenStream) -> TokenStream {
-    let item: Item = syn::parse(input).unwrap();
-    let expanded = render::quote(item);
-    expanded.into()
-}
 
 #[proc_macro_derive(Lifecycle)]
 pub fn derive_lifecycle(input: TokenStream) -> TokenStream {
