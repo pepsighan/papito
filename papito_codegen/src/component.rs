8f6e0a42b6661f23038f277c9b1effd814baf5be
Sharad Chand <sharad.d.chand@gmail.com>
2018-04-03 08:39:52 +0545

    Added impl for the data, setters and getters

--- papito_codegen/src/component.rs
+++ papito_codegen/src/component.rs
@@ -1,5 +1,5 @@
-use syn::{Ident, Type, Path, Item, ItemStruct, Visibility, Attribute, Fields, Field};
 use quote::Tokens;
+use syn::{Attribute, Field, Fields, Ident, Item, ItemStruct, Path, Type, Visibility};
 
 pub fn quote(item: Item) -> Tokens {
     match item {
@@ -7,9 +7,10 @@ pub fn quote(item: Item) -> Tokens {
             let component_data = ComponentData::parse(&item_struct);
             let mut component_struct = ComponentStruct::parse(&item_struct);
             if let Some(ref comp_data) = component_data {
-                component_struct.set_data(comp_data.ident.clone());
+                component_struct.set_data(comp_data.data.clone());
             }
             let component_struct = component_struct.quote();
+            let component_data = component_data.map(|it| it.quote());
 
             quote! {
                 #component_struct
@@ -24,7 +25,7 @@ pub fn quote(item: Item) -> Tokens {
 struct ComponentStruct {
     attrs: Vec<Attribute>,
     vis: Visibility,
-    ident: Ident,
+    component: Ident,
     data: Option<Ident>,
 }
 
@@ -32,11 +33,11 @@ impl ComponentStruct {
     fn parse(item: &ItemStruct) -> ComponentStruct {
         let attrs = item.attrs.clone();
         let vis = item.vis.clone();
-        let ident = item.ident.clone();
+        let component = item.ident.clone();
         ComponentStruct {
             attrs,
             vis,
-            ident,
+            component,
             data: None,
         }
     }
@@ -48,23 +49,31 @@ impl ComponentStruct {
     fn quote(self) -> Tokens {
         let attrs = self.attrs;
         let vis = self.vis;
-        let ident = self.ident;
+        let component = self.component;
         if let Some(data) = self.data {
             quote! {
-                #vis struct #ident {
-                    _data: ::std::rc::Rc<::std::cell::RefCell<#data>>
+                #vis struct #component {
+                    _data: ::std::rc::Rc<::std::cell::RefCell<#data>>,
+                    _notifier: Box<Fn()>
+                }
+
+                impl #component {
+                    fn _notify(&self) {
+                        (self._notifier)();
+                    }
                 }
             }
         } else {
             quote! {
-                #vis struct #ident;
+                #vis struct #component;
             }
         }
     }
 }
 
 struct ComponentData {
-    ident: Ident,
+    data: Ident,
+    props: Ident,
     fields: DataFields,
     component: Ident,
 }
@@ -74,14 +83,50 @@ impl ComponentData {
         let data_fields = DataFields::parse(&item.fields);
         if let Some(fields) = data_fields {
             let component = item.ident.clone();
-            let ident = Ident::from(format!("{}Data", &component));
+            let data = Ident::from(format!("_{}Data", &component));
+            let props = Ident::from(format!("_{}Props", &component));
             Some(ComponentData {
-                ident,
+                data,
+                props,
                 component,
                 fields,
             })
         } else {
             None
+        }
+    }
+
+    fn quote(self) -> Tokens {
+        let data = self.data;
+        let props = self.props;
+        let component = self.component;
+        let data_fields = self.fields.quote_data();
+        let props_fields = self.fields.quote_props();
+        let data_getters = self.fields.quote_getters();
+        let component_getters = self.fields.quote_component_getters();
+        let data_setters = self.fields.quote_setters();
+        let component_setters = self.fields.quote_component_setters();
+
+        quote! {
+            struct #props {
+                #props_fields
+            }
+
+            struct #data {
+                #data_fields
+
+                #data_setters
+            }
+
+            impl #data {
+                #data_getters
+            }
+
+            impl #component {
+                #component_getters
+
+                #component_setters
+            }
         }
     }
 }
@@ -107,12 +152,66 @@ impl DataFields {
             }
         }
     }
+
+    fn quote_data(&self) -> Tokens {
+        let fields: Vec<_> = self.fields.iter()
+            .map(|it| it.quote_data())
+            .collect();
+        quote! {
+            #(#fields),*
+        }
+    }
+
+    fn quote_props(&self) -> Tokens {
+        let fields: Vec<_> = self.fields.iter()
+            .map(|it| it.quote_props())
+            .collect();
+        quote! {
+            #(#fields),*
+        }
+    }
+
+    fn quote_getters(&self) -> Tokens {
+        let getters: Vec<_> = self.fields.iter()
+            .map(|it| it.quote_getters())
+            .collect();
+        quote! {
+            #(#getters),*
+        }
+    }
+
+    fn quote_component_getters(&self) -> Tokens {
+        let getters: Vec<_> = self.fields.iter()
+            .map(|it| it.quote_component_getters())
+            .collect();
+        quote! {
+            #(#getters),*
+        }
+    }
+
+    fn quote_setters(&self) -> Tokens {
+        let setters: Vec<_> = self.fields.iter()
+            .map(|it| it.quote_setters())
+            .collect();
+        quote! {
+            #(#setters),*
+        }
+    }
+
+    fn quote_component_setters(&self) -> Tokens {
+        let setters: Vec<_> = self.fields.iter()
+            .map(|it| it.quote_component_setters())
+            .collect();
+        quote! {
+            #(#setters),*
+        }
+    }
 }
 
 struct DataField {
     ident: Ident,
     ty: Type,
-    is_prop: bool
+    is_prop: bool,
 }
 
 impl DataField {
@@ -123,7 +222,83 @@ impl DataField {
         DataField {
             ident: field.ident.as_ref().unwrap().clone(),
             ty: field.ty.clone(),
-            is_prop: field.attrs.has_prop_attribute()
+            is_prop: field.attrs.has_prop_attribute(),
+        }
+    }
+
+    fn quote_data(&self) -> Tokens {
+        let ident = &self.ident;
+        let ty = &self.ty;
+        quote! {
+            #ident: #ty
+        }
+    }
+
+    fn quote_props(&self) -> Option<Tokens> {
+        if self.is_prop {
+            let ident = &self.ident;
+            let ty = &self.ty;
+            Some(quote! {
+                #ident: #ty
+            })
+        } else {
+            None
+        }
+    }
+
+    fn quote_getters(&self) -> Tokens {
+        let ident = &self.ident;
+        let ty = &self.ty;
+        quote! {
+            fn #ident(&self) -> #ty {
+                self.#ident.clone()
+            }
+        }
+    }
+
+    fn quote_component_getters(&self) -> Tokens {
+        let ident = &self.ident;
+        let ty = &self.ty;
+        quote! {
+            fn #ident(&self) -> #ty {
+                self._data.borrow().#ident()
+            }
+        }
+    }
+
+    fn quote_setters(&self) -> Option<Tokens> {
+        if self.is_prop {
+            let ident = &self.ident;
+            let ty  = &self.ty;
+            Some(quote! {
+                fn set_#ident(&mut self, value: #ty) -> bool {
+                    if self.#ident != value {
+                        self.#ident = value;
+                        true
+                    } else {
+                        false
+                    }
+                }
+            })
+        } else {
+            None
+        }
+    }
+
+    fn quote_component_setters(&self) -> Option<Tokens> {
+        if self.is_prop {
+            let ident = &self.ident;
+            let ty = &self.ty;
+            Some(quote! {
+                fn set_#ident(&self, value: #ty) {
+                    let changed = self._data.borrow_mut().set_#ident(value);
+                    if changed {
+                        self._notify();
+                    }
+                }
+            })
+        } else {
+            None
         }
     }
 }
